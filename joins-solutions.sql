007cc380ef8015b38ce46d9700be6f8d887f789f
anwar <anwarhussein2353@gmail.com>
2021-09-28 14:26:32 -0500

    Added customers and products count

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -17,21 +17,22 @@ ON products.id = warehouse_product.product_id
 JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
 WHERE products.description = 'cheetos'
 
-
 -- Which warehouses have diet pepsi?
 SELECT * FROM warehouse_product JOIN products 
 ON products.id = warehouse_product.product_id
 JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
 WHERE products.description = 'diet pepsi'
 
-
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
+SELECT * FROM addresses JOIN customers ON customers.id = addresses.customer_id JOIN orders ON orders.id = orders.address_id
 
 -- How many customers do we have?
-
+SELECT COUNT(*) FROM customers
 
 -- How many products do we carry?
+SELECT COUNT(*) FROM products
+
 -- What is the total available on-hand quantity of diet pepsi?
 
 Stretch
